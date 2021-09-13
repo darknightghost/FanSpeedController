39c75fe16acebf011ea394b9f0c9c7e641fd986e
Wang Siyuan <darknightghost.cn@gmail.com>
2021-09-13 22:27:29 +0800

    UTC:2021-09-13 14:27:29 :

--- firmware/include/platform.h
+++ firmware/include/platform.h
@@ -2,6 +2,15 @@
 
 #include <stdint.h>
 
+#if defined EDITOR_AUTO_COMPLETE
+    // Auto-complete.
+    #define data
+    #define idata
+    #define xdata
+    #define __at(addr)
+
+#endif
+
 /**
  * @brief       Register data type.
  */
@@ -16,23 +25,25 @@ union register_t {
         uint8_t bit5 : 1; ///< Bit 5.
         uint8_t bit6 : 1; ///< Bit 6.
         uint8_t bit7 : 1; ///< Bit 7.
-    } __attribute__((packed)) bits;
-} __attribute__((packed));
+    } bits;
+};
 
-_Static_assert(sizeof(register_t) == 1);
+#define REGISTER(addr, name) __at(addr) union register_t name
+
+_Static_assert(sizeof(union register_t) == 1);
 
 // Definations of STC8G1K08-36I-SOP8
 // I/O ports.
 // P3
-#define P3_REG (*((union register_t *)(0xB0)))
-#define P3     (P3_REG.value)
-#define P3_0   (P3_REG.bits.bit0)
-#define P3_1   (P3_REG.bits.bit1)
-#define P3_2   (P3_REG.bits.bit2)
-#define P3_3   (P3_REG.bits.bit3)
+REGISTER(0xB0, P3_REG);
+#define P3   (P3_REG.value)
+#define P3_0 (P3_REG.bits.bit0)
+#define P3_1 (P3_REG.bits.bit1)
+#define P3_2 (P3_REG.bits.bit2)
+#define P3_3 (P3_REG.bits.bit3)
 
 // P5
-#define P5_REG (*((union register_t *)(0xC8)))
-#define P5     (P5_REG.value)
-#define P5_4   (P5_REG.bits.bit4)
-#define P5_5   (P5_REG.bits.bit5)
+REGISTER(0xC8, P5_REG);
+#define P5   (P5_REG.value)
+#define P5_4 (P5_REG.bits.bit4)
+#define P5_5 (P5_REG.bits.bit5)
--- firmware/source/main.c
+++ firmware/source/main.c
@@ -3,7 +3,6 @@
 int main()
 {
     while (1) {
-    };
-
-    return 0;
+        P3_2 = P3_3;
+    }
 }
